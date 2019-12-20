3555279da58836b24411894538e576670b3cd925
shaadhussain <hussai19@purdue.edu>
2019-12-19 21:49:34 -0600

    Added full bookmarking fxnality

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ function saveBookmark(e) {
 
     var siteName = document.getElementById("siteName").value;
     var siteURL = document.getElementById("siteURL").value;
-    console.log(siteName);  
 
     var bookmark = {
         name:siteName, 
@@ -15,10 +14,57 @@ function saveBookmark(e) {
 
     //Local Storage
 
-    localStorage.setItem('test', 'HW');
-    console.log(localStorage.getItem('test'));
+    // localStorage.setItem('test', 'HW');
+    // console.log(localStorage.getItem('test'));
+    // localStorage.removeItem('test');
+    // console.log(localStorage.getItem('test'));
 
-    console.log(bookmark);
+
+
+  //  console.log(bookmark);
+
+  //Test if bookmarks is null   
+  if (localStorage.getItem('bookmarks') === null) {
+      var bookmarks = [];
+      bookmarks.push(bookmark);
+      localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
+  else {
+      var bookmarks = JSON.parse(localStorage.getItem('bookmarks')); //THIS IS STRING   
+    //Add bookmark to array
+    bookmarks.push(bookmark);
+
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+  
+    }
     //Prevent form submission
     e.preventDefault();
 }
+
+function fetchBookmarks() {
+
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks')); //THIS IS STRING   
+
+    console.log(bookmarks);
+
+    //Get output id
+    var bookmarksResults = document.getElementById('bookmarksResults');
+
+    //Build output
+    bookmarksResults.innerHTML = '';
+
+    for (var i = 0; i < bookmarks.length; i++) {
+        var name = bookmarks[i].name;
+        var url = bookmarks[i].url;
+
+        bookmarksResults.innerHTML += '<div class="well">' +
+                                        '<h3>' + name + 
+                                        ' <a class="btn btn-default" target="_blank" href="'+ 
+                                        url + '"> Visit </a> '
+                                        '</h3>'
+                                        + '</div>'     
+    }
+
+
+}   
